a95331594463cde7cb0a2a550361dc7173df7a71
Sazhin-Kostya <112079591+Sazhin-Kostya@users.noreply.github.com>
2023-07-12 06:57:06 +0000

    Deploying to gh-pages from @ Sazhin-Kostya/goit-js-hw-09@520dd38e4b02097376cca887f6ed4fa2942cece2 🚀

--- 01-color-switcher.791efe28.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");e.addEventListener("click",(()=>{timerId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!0})),t.addEventListener("click",(()=>{clearInterval(timerId),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.791efe28.js.map
